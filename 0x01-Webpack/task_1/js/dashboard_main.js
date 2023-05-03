499bf53b9acfe7e73e4a206ec273e9cdedfe2352
axel <axel.valentinbvs@yahoo.com>
2023-05-03 10:08:02 +0000

    add tasj

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from "jquery";
+import _ from "lodash";
+
+let count = 0;
+
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
+
+function updateCounter() {
+  let counter = parseInt($("#count").text()) || 0;
+  counter++;
+  $("#count").html(`${counter} clicks on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
