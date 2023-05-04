7874be10af73c8f37c90475cca5dcd9389bae24d
axel <axel.valentinbvs@yahoo.com>
2023-05-04 09:32:22 +0000

    upadte

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,6 +1,8 @@
 import $ from "jquery";
 import _ from "lodash";
 
+let count = 0;
+
 $("body").append("<p>Holberton Dashboard</p>");
 $("body").append("<p>Dashboard data for the students</p>");
 $("body").append("<button>Click here to get started</button>");
@@ -8,9 +10,10 @@ $("body").append("<p id='count'></p>");
 $("body").append("<p>Copyright - Holberton School</p>");
 
 function updateCounter() {
-  let count = parseInt($("#count").text()) || 0;
   count++;
-  $("#count").html(`${count} clicks on the button`);
+  $("#count").text(`${count} clicks on the button`);
 }
 
-$("button").on("click", _.debounce(updateCounter, 500));
+$(() => {
+  $("button").on("click", _.debounce(updateCounter, 500));
+});
