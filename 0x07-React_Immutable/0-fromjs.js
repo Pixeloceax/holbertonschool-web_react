f68adf5e5665ad6e5402f98ba3ddba7886933130
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-06-13 17:35:22 +0000

    add task

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,14 +1,7 @@
 import { fromJS } from 'immutable';
 
-/* const object = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
-};
- */
-export default function getImmutableObject(object) {
+function getImmutableObject(object) {
   return fromJS(object);
 }
-/* 
-console.log(getImmutableObject(object)); */
+
+export default getImmutableObject;
